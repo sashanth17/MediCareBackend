5ee95aeb7e44e1c5251e70799092b019d086c95c
sashanth17 <sashan1707@gmail.com>
2025-09-11 12:24:36 +0530

    initialized repo

---
+++ MediCareBackend/UserDetails/models.py
@@ -0,0 +1,33 @@
+# models.py
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from datetime import date
+
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    gender = models.CharField(max_length=8, choices=[
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ], blank=True, null=True)
+    blood_group = models.CharField(max_length=3, choices=[
+        ('A+', 'A+'), ('A-', 'A-'),
+        ('B+', 'B+'), ('B-', 'B-'),
+        ('AB+', 'AB+'), ('AB-', 'AB-'),
+        ('O+', 'O+'), ('O-', 'O-'),
+    ], blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def age(self):
+        """Return age based on date_of_birth"""
+        if self.date_of_birth:
+            today = date.today()
+            return (
+                today.year - self.date_of_birth.year
+                - ((today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day))
+            )
+        return None

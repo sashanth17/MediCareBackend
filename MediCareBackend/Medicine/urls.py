cc8e9839ca7ce6bb4693faed8673cc4317ed4747
SOUNDAR42 <soundar422006@gmail.com>
2025-11-26 15:54:18 +0530

    added the name based search url

--- MediCareBackend/Medicine/urls.py
+++ MediCareBackend/Medicine/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-from .views import MedicineListView,MedicineDetailView
+from .views import MedicineListView,MedicineDetailView,MedicineByNameView
 urlpatterns=[
     path('', MedicineListView.as_view(), name='medicine-list'),
-    path("<str:medicine_id>/", MedicineDetailView.as_view(), name="medicine-detail")
+    path("<str:medicine_id>/", MedicineDetailView.as_view(), name="medicine-detail"),
+    path('name/<str:medicine_name>/', MedicineByNameView.as_view(), name='medicine-by-name'),
 ]
--- MediCareBackend/Medicine/views.py
+++ MediCareBackend/Medicine/views.py
@@ -1,6 +1,7 @@
 # views.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Medicine
 from .serializers import MedicineSerializer,MedicineInstance,MedicineInstanceSerializer
 
@@ -14,6 +15,21 @@ class MedicineDetailView(APIView):
     def get(self, request, medicine_id):
         try:
             medicine = Medicine.objects.get(medicine_id=medicine_id)
+        except Medicine.DoesNotExist:
+            return Response({"error": "Medicine not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        instances = MedicineInstance.objects.filter(medicine=medicine)
+
+        return Response({
+            "medicine": MedicineSerializer(medicine).data,
+            "instances": MedicineInstanceSerializer(instances, many=True).data
+        })
+
+class MedicineByNameView(APIView):
+    def get(self, request, medicine_name):
+        try:
+            # Match the correct DB field
+            medicine = Medicine.objects.get(medicine_name__iexact=medicine_name)
         except Medicine.DoesNotExist:
             return Response({"error": "Medicine not found"}, status=status.HTTP_404_NOT_FOUND)
 
